226321993ea0b5728c67ac225bbe03fef2217062
AlenaKorshunova <139872029+AlenaKorshunova@users.noreply.github.com>
2024-12-22 12:52:13 +0300

    Update app.py
    добавляем функцию для получения ключа местоположения и координат по названию города

--- app.py
+++ app.py
@@ -24,3 +24,23 @@ class Response(Enum):
     BAD = auto()
     USER_ERROR = auto()
     SERVER_ERROR = auto()
+
+
+def get_location_key_and_coord(city: str):
+    """Получить ключ местоположения и координаты по названию города."""
+    url = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    params = {'apikey': api_key, 'q': city}
+    
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        try:
+            data = response.json()[0]
+            location_key = data['Key']
+            coord = (data['GeoPosition']['Latitude'], data['GeoPosition']['Longitude'])
+            return location_key, coord, response.status_code
+        except (IndexError, KeyError):
+            return None, None, Response.BAD
+    else:
+        print(f'Error: {response.status_code}')
+        return None, None, Response.SERVER_ERROR
