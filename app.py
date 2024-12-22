1dc7b0055bc1377af0a7281ab66b1620b2373c69
AlenaKorshunova <139872029+AlenaKorshunova@users.noreply.github.com>
2024-12-22 12:51:10 +0300

    Update app.py
    добавляет api key , создаем глобальные переменные

--- app.py
+++ app.py
@@ -9,3 +9,18 @@ from dash import Dash, dcc, html, Input, Output, callback_context, ctx
 import dash_leaflet as dl
 import plotly.graph_objs as go
 from dash import ALL
+
+
+# Загрузка переменных окружения
+load_dotenv()
+api_key = "UpvLidZdnGcQAhDJN6286dwoe7SZvSwJ"
+
+# Глобальные переменные для хранения данных о местоположении и погоде
+data_loc = {}
+data_weather = {}
+
+class Response(Enum):
+    GOOD = auto()
+    BAD = auto()
+    USER_ERROR = auto()
+    SERVER_ERROR = auto()
